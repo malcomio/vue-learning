9b8e9e5c11db64d3e577592910136e4407bfbdfc
Malcolm Young <428782+malcomio@users.noreply.github.com>
2021-11-18 15:05:52 +0000

    exclude API from manifest

--- vue.config.js
+++ vue.config.js
@@ -1,26 +1,29 @@
 process.env.VUE_APP_VERSION = process.env.npm_package_version;
 
-const { InjectManifest } = require('workbox-webpack-plugin');
+const {InjectManifest} = require('workbox-webpack-plugin');
 
 module.exports = {
     publicPath: '/vue/',
     // remove workbox from vue-pwa pipeline
     chainWebpack: config => {
-      config.plugins.delete('workbox');
+        config.plugins.delete('workbox');
     },
     configureWebpack: () => {
-      if (process.env.NODE_ENV === "production") {
-        return {
-          plugins: [
-            // add workbox to webpack config.
-            new InjectManifest({
-              swSrc: "./src/service-worker.js",
-              swDest: "service-worker.js",
-              // other workbox options..
-            }),
-          ]
-        };
-      }
+        if (process.env.NODE_ENV === "production") {
+            return {
+                plugins: [
+                    // add workbox to webpack config.
+                    new InjectManifest({
+                        swSrc: "./src/service-worker.js",
+                        swDest: "service-worker.js",
+                        exclude: [
+                            /^.*api\/.*$/,
+                        ]
+                        // other workbox options..
+                    }),
+                ]
+            };
+        }
     },
     devServer: {
         host: 'localhost',
@@ -28,18 +31,18 @@ module.exports = {
         overlay: true
     },
     pwa: {
-      manifestOptions: {
-        name: "Vue learning repo",
-        short_name: "Vue learning",
-        start_url: "/",
-        display: "standalone",
-        theme_color: "#1f70b8",
-        background_color: "#1f70b8",
-      },
-      themeColor: "#1f70b8",
-      msTileColor: "#1f70b8",
-      appleMobileWebAppCapable: "yes",
-      appleMobileWebAppStatusBarStyle: "black",
-      manifestCrossorigin: 'use-credentials',
+        manifestOptions: {
+            name: "Vue learning repo",
+            short_name: "Vue learning",
+            start_url: "/",
+            display: "standalone",
+            theme_color: "#1f70b8",
+            background_color: "#1f70b8",
+        },
+        themeColor: "#1f70b8",
+        msTileColor: "#1f70b8",
+        appleMobileWebAppCapable: "yes",
+        appleMobileWebAppStatusBarStyle: "black",
+        manifestCrossorigin: 'use-credentials',
     },
-  };
+};
