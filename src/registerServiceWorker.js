ebccdf1b68d4f56d832852cfb186c15d708e1fd7
Malcolm Young <428782+malcomio@users.noreply.github.com>
2021-11-18 15:09:02 +0000

    add basic notifications

--- src/registerServiceWorker.js
+++ src/registerServiceWorker.js
@@ -2,10 +2,8 @@
 
 import { register } from 'register-service-worker'
 
-console.log(process);
-
 if (process.env.NODE_ENV === 'production') {
-  register(`${process.env.BASE_URL}service-worker.js`, {
+  register(`${process.env.BASE_URL}service-worker.js?a`, {
     ready () {
       console.log(
         'App is being served from cache by a service worker.\n' +
@@ -25,10 +23,39 @@ if (process.env.NODE_ENV === 'production') {
       console.log('New content is available; please refresh.')
     },
     offline () {
+      // TODO: why doesn't this actually fire?
       console.log('No internet connection found. App is running in offline mode.')
+      displayNotification('No internet connection found. App is running in offline mode.', 'banana')
     },
     error (error) {
       console.error('Error during service worker registration:', error)
     }
+  })
+}
+
+const notificationsSupported = () =>
+    'Notification' in window &&
+    'serviceWorker' in navigator &&
+    'PushManager' in window;
+
+function displayNotification(body, title) {
+  if (notificationsSupported() && Notification.permission === 'granted') {
+    const options = {
+      body: body
+    };
+
+    navigator.serviceWorker.getRegistration().then(reg => {
+      reg.showNotification(title, options).then(notification => {
+        console.log(notification);
+      });
+    });
+  }
+}
+
+// TODO: Firefox needs this to happen from an explicit user action.
+if (notificationsSupported() && Notification.permission !== "granted") {
+  Notification.requestPermission().then(permission => {
+    console.log(permission);
+    displayNotification("You are now set up to use notifications", 'Vue');
   })
 }
