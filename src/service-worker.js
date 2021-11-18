ed0f369e4361aaa56a6f77a12382331a6c84e6dd
Malcolm Young <428782+malcomio@users.noreply.github.com>
2021-11-18 15:08:06 +0000

    move more specific POST route registration before GET

--- src/service-worker.js
+++ src/service-worker.js
@@ -1,20 +1,13 @@
-import { Plugin } from 'workbox-background-sync';
-import { precacheAndRoute } from 'workbox-precaching';
-import { registerRoute } from 'workbox-routing';
-import { NetworkFirst, NetworkOnly } from 'workbox-strategies';
+import {Plugin} from 'workbox-background-sync';
+import {precacheAndRoute} from 'workbox-precaching';
+import {registerRoute} from 'workbox-routing';
+import {NetworkFirst, NetworkOnly} from 'workbox-strategies';
 
 // Use with precache injection
 precacheAndRoute(self.__WB_MANIFEST);
 
-// Cache local requests.
-registerRoute(
-    ({url}) => url.origin === self.location.origin,
-    new NetworkFirst(),
-    'GET'
-);
-
 // Sync POSTs when we get network back.
-const bgSyncPlugin = new Plugin('ehc-offline-queue', { // eslint-disable-line
+const bgSyncPlugin = new Plugin('ehc-offline-queue', {
     maxRetentionTime: 24 * 60 // Retry for max of 24 Hours (specified in minutes)
 });
 
@@ -24,4 +17,14 @@ registerRoute(
         plugins: [bgSyncPlugin]
     }),
     'POST'
+);
+
+// Cache local requests.
+registerRoute(
+    ({url}) => {
+        return url.origin === self.location.origin
+        && !url.pathname.startsWith(('/vue/api'))
+    },
+    new NetworkFirst(),
+    'GET'
 );
